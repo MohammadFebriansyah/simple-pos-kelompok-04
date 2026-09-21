f9310c10e3f2f3d067c48ee1ced7ff140b24e189
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2026-09-21 14:52:16 +0700

    Membuat tampilan riwayat transaksi

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -25,9 +25,9 @@ class TransactionController extends Controller
         return 'Transaksi disimpan (belum ada logika penyimpanan)';
     }
 
-    public function index()
+   public function index()
     {
-        return 'Daftar transaksi';
+        return view('transactions.index');
     }
 
     public function show(string $id)
