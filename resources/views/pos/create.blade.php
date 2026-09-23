96918fdef5b968cfe3ccf77c582c0da30498c6ef
Derikho <hpkuhuaweip9@gmail.com>
2026-09-23 12:34:41 +0700

    Tambahkan tombol Hapus pada tiap baris item di keranjang

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -10,6 +10,9 @@
     addToCart(id, name, price) {
         this.cart.push({ id, name, price });
     },
+    removeFromCart(id) {
+        this.cart = this.cart.filter(item => item.id !== id);
+    },
     subtotal() {
         return this.cart.reduce((sum, item) => sum + item.price, 0);
     }
@@ -34,7 +37,16 @@
 
     <div class="mt-4 border-t pt-3">
         <template x-for="item in cart" :key="item.id">
-            <p x-text="item.name + ' - Rp ' + item.price"></p>
+            <div class="flex items-center justify-between gap-2">
+                <p x-text="item.name + ' - Rp ' + item.price"></p>
+                <button
+                    type="button"
+                    class="text-red-600 hover:text-red-800"
+                    @click="removeFromCart(item.id)"
+                >
+                    Hapus
+                </button>
+            </div>
         </template>
         
         <p class="font-semibold mt-2">
