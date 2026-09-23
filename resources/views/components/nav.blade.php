0fc0d90e2816796a616813e8db678c10647c1ec4
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2026-09-23 14:40:25 +0700

    rapikan nav dan tandai link aktif

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,9 +1,15 @@
 <div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-
-    <nav class="bg-slate-900 text-white px-4 py-3 flex gap-4">
+    <nav class="bg-slate-900 text-white px-4 py-3 flex gap-6">
         <span class="font-semibold">Simple POS</span>
-        <a href="{{ route('pos.create') }}" class="hover:underline">Kasir</a>
-        <a href="{{ route('transactions.index') }}" class="hover:underline">Transaksi</a>
+
+        <a href="{{ route('pos.create') }}"
+           class="{{ request()->routeIs('pos.create') ? 'font-bold underline' : 'hover:underline' }}">
+            Kasir
+        </a>
+
+        <a href="{{ route('transactions.index') }}"
+           class="{{ request()->routeIs('transactions.index') ? 'font-bold underline' : 'hover:underline' }}">
+            Transaksi
+        </a>
     </nav>
 </div>
